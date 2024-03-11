49a5aed6314ab82df0b07d13256756ad73afe7ad
alihoushngi <blumy.info@fmail.com>
2024-03-11 15:06:16 +0330

    fonts added , navbar created , search section created

--- src/scripts/navbar.js
+++ src/scripts/navbar.js
@@ -1,4 +1,4 @@
-// ? create array for navbar list
+// ? create array for navbar list ----------------
 const navbarArray = [
   {
     name: "خانه",
@@ -14,21 +14,36 @@ const navbarArray = [
   },
 ];
 
-// ! get navbar from dom
+// ! get navbar from dom -------------------------
 const navbar = document.querySelector("#navbar");
-navbar.classList.add("w-full");
+// create elements for menu ----------------------
 const menuWrapper = document.createElement("div");
 const leftSideWrapper = document.createElement("div");
 const navbarList = document.createElement("ul");
+
+// add class to my items ------------------------
+navbar.classList.add("w-full");
 navbarList.classList.add("flex", "gap-3", "p-5");
+
+// create navbar item with navbarArray ----------
 navbarArray.forEach((item) => {
   const list = document.createElement("li");
   const link = document.createElement("a");
+
+  // a tag link to list -------------------------
   list.appendChild(link);
+
+  // a tag get items text and href --------------
   link.textContent = item.name;
   link.href = item.link;
+
+  // list link to navbar ------------------------
   navbarList.appendChild(list);
 });
+
+// ul add to navbarWrapper and navbarWrapper added to navbar ----
 menuWrapper.appendChild(navbarList);
 navbar.appendChild(menuWrapper);
+
+//left side of navbar linked to navbar ---------
 navbar.appendChild(leftSideWrapper);
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,10 @@
 module.exports = {
   content: ["**/*.{html,js}"],
   theme: {
+    colors: {
+      primaryColor: "#9e1817",
+      secondaryColor: "#777e80",
+    },
     extend: {},
   },
   plugins: [],
